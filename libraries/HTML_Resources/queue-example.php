750d98f709e48fd3cb91d3a92e6cc9ace6aeec37
jayofearth <jayofearth@gmail.com>
2014-01-27 13:09:46 -0500

    Update queue-example.php

--- libraries/HTML_Resources/queue-example.php
+++ libraries/HTML_Resources/queue-example.php
@@ -2,19 +2,19 @@
 
 include 'HTML_Resources.php';
 
-$this->HTML_Resources = new HTML_Resources;
+$HTML_Resources = new HTML_Resources;
 
 /**
  * Queuing a Stylesheet
  */
-$this->HTML_Resources->queue_stylesheet( 'stylesheet.css' );
+$HTML_Resources->queue_stylesheet( 'stylesheet.css' );
 
 /**
  * Queuing a Script for the <head> tag
  */
-$this->HTML_Resources->queue_script( 'javascript-file.js', 'header' );
+$HTML_Resources->queue_script( 'javascript-file.js', 'header' );
 
 /**
  * Queuing a Script for the end of the <body> tag
  */
-$this->HTML_Resources->queue_script( 'javascript-file.js', 'footer' );
+$HTML_Resources->queue_script( 'javascript-file.js', 'footer' );
